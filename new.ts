8e94f1e77620f47b1b5ae8d50914d7ec24de2cc3
ArshadAliJahejo <162421282+ArshadAliJahejo@users.noreply.github.com>
2024-03-05 23:50:56 -0800

    Add files via upload

---
+++ new.ts
@@ -0,0 +1,19 @@
+let persentage = '71'
+if (persentage === "95"){
+    console.log('congratulations you have pass with A1 grade')
+}
+
+else if (persentage === '80'){
+    console.log('congratulations you have pass with A grade')
+    
+}
+else if (persentage === '71') {
+    console.log('congratulations you have pass with B grade')
+
+}
+else if (persentage === '55'){
+    console.log('congratulations you have pass with C grade')
+
+}
+
+else console.log ('sorry you dont pass this exam')
